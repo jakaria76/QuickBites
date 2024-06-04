ea49c2a1189df285f41d47f8e238bd7090ad2087
Md.Redwan Hossen <me.redwanhossen@gmail.com>
2024-06-04 21:18:30 +0600

    same as before

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
-import { useState } from "react"
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-const App = () => {
-  let heroData = [
-    {text1:"Dive info",text2:"what you love"},
-    {text1:"Indulge",text2:"your passionns"},
-    {text1:"Give in to",text2:"your passionns"},
-  ]
-  const [heroCount,setHeroCount] = useState(2);
-  const [playStatus, setPlayStatus] = useState(true);
-  return(
-    <div>
-      <Background playStatus={playStatus} heroCount={heroCount}/>
-    </div>
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
 
